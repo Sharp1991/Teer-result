68de481ac37a7cf61e6eb72400e2e1e6ce70a8c9
Sharp1991 <shaphrang.syiem@gmail.com>
2025-11-17 16:08:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: true, // Enables React/SWC compiler
-};
+  experimental: {
+    appDir: true,
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
